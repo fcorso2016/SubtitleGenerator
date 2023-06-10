c6f5f7aee5218b47ca7bc586fef7d732508dc908
Francesco Corso <fcorso2016@gmail.com>
2023-06-10 11:52:53 -0400

    Filled out the converter

--- FileConverter/WavConverter.cpp
+++ FileConverter/WavConverter.cpp
@@ -3,10 +3,40 @@
 //
 #include "WavConverter.h"
 
-#include <QProcess>
 
-WavConverter::WavConverter() = default;
+WavConverter::WavConverter(QObject *parent) : QObject(parent) {
+    connect(this, &WavConverter::conversionFinished, this, &WavConverter::onConversionFinished);
+}
+
+WavConverter::~WavConverter() = default;
 
 void WavConverter::convertFile(std::string_view filename) {
-    QProcess ffmpeg;
+    QString inputFile(filename.data());
+
+    QFileInfo inputInfo(inputFile);
+    QString outputFile = m_conversionDir.path() + QDir::separator() + inputInfo.baseName() + ".wav";
+
+    auto ffmpeg = std::make_unique<QProcess>();
+    ffmpeg->setProgram("ffmpeg");
+    ffmpeg->setArguments({"-i", inputFile, "-ac", "2", "-f", "wav", outputFile});
+    connect(ffmpeg.get(), SIGNAL(finished), this, SIGNAL(conversionFinished));
+    queueConversion(std::move(ffmpeg));
+}
+
+void WavConverter::queueConversion(std::unique_ptr<QProcess> process) {
+    m_processes.emplace(std::move(process));
+    if (m_processes.size() == 1) {
+        const auto &ffmpeg = m_processes.front();
+        ffmpeg->start();
+        emit conversionStarted();
+    }
+}
+
+void WavConverter::onConversionFinished() {
+    m_processes.pop();
+    if (!m_processes.empty()) {
+        const auto &ffmpeg = m_processes.front();
+        ffmpeg->start();
+        emit conversionStarted();
+    }
 }
--- FileConverter/WavConverter.h
+++ FileConverter/WavConverter.h
@@ -4,13 +4,27 @@
 #pragma once
 
 #include <QTemporaryDir>
+#include <QProcess>
+#include <queue>
 
-class WavConverter {
+class WavConverter : public QObject {
+    Q_OBJECT
 public:
-    WavConverter();
+    explicit WavConverter(QObject *parent);
+    ~WavConverter() override;
 
     void convertFile(std::string_view filename);
 
+signals:
+    void conversionStarted();
+    void conversionFinished(int exitCode, QProcess::ExitStatus exitStatus);
+
+private slots:
+    void onConversionFinished();
+
 private:
+    void queueConversion(std::unique_ptr<QProcess> process);
+
     QTemporaryDir m_conversionDir;
+    std::queue<std::unique_ptr<QProcess>> m_processes;
 };
